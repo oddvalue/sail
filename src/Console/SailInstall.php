9cc4c0feeb9917e2b91a6b148e91a8cd92863a1e
Jim Hollington <jim@oddvalue.co.uk>
2020-12-12 20:34:44 +0000

    Make commands compatible with older versions

---
+++ src/Console/SailInstall.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Laravel\Sail\Console;
+
+use Illuminate\Console\Command;
+
+class SailInstall extends Command
+{
+    protected $description = 'Install Laravel Sail\'s default Docker Compose file';
+
+    protected $signature = 'sail:install';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        copy(__DIR__.'/../../stubs/docker-compose.yml', base_path('docker-compose.yml'));
+
+        $environment = file_get_contents(base_path('.env'));
+
+        $environment = str_replace('DB_HOST=127.0.0.1', 'DB_HOST=mysql', $environment);
+        $environment = str_replace('MEMCACHED_HOST=127.0.0.1', 'MEMCACHED_HOST=redis', $environment);
+        $environment = str_replace('REDIS_HOST=127.0.0.1', 'REDIS_HOST=redis', $environment);
+
+        file_put_contents(base_path('.env'), $environment);
+    }
+}
---
+++ src/Console/SailPublish.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Laravel\Sail\Console;
+
+use Illuminate\Console\Command;
+
+class SailPublish extends Command
+{
+    protected $description = 'Publish the Laravel Sail Docker files';
+
+    protected $signature = 'sail:publish';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $this->call('vendor:publish', ['--tag' => 'sail']);
+
+        file_put_contents(base_path('docker-compose.yml'), str_replace(
+            './vendor/laravel/sail/runtimes/8.0',
+            './docker/8.0',
+            file_get_contents(base_path('docker-compose.yml'))
+        ));
+    }
+}
