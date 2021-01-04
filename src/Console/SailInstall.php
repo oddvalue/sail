0c4a4aaf6e9d71fb72eab4fb352667a54ad5e4f2
Jim <jim@oddvalue.co.uk>
2021-01-04 11:54:59 +0000

    Fix memcache

--- src/Console/SailInstall.php
+++ src/Console/SailInstall.php
@@ -22,7 +22,7 @@ class SailInstall extends Command
         $environment = file_get_contents(base_path('.env'));
 
         $environment = str_replace('DB_HOST=127.0.0.1', 'DB_HOST=mysql', $environment);
-        $environment = str_replace('MEMCACHED_HOST=127.0.0.1', 'MEMCACHED_HOST=redis', $environment);
+        $environment = str_replace('MEMCACHED_HOST=127.0.0.1', 'MEMCACHED_HOST=memcached', $environment);
         $environment = str_replace('REDIS_HOST=127.0.0.1', 'REDIS_HOST=redis', $environment);
 
         file_put_contents(base_path('.env'), $environment);
